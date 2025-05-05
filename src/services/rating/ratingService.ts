b7888c6131591151f67d05fff6ce0dae8e7c0608
Ho Tolosa <34034036+htolosam@users.noreply.github.com>
2025-05-04 22:24:57 -0500

    refactor: vista chat

--- src/services/rating/ratingService.ts
+++ src/services/rating/ratingService.ts
@@ -5,8 +5,18 @@ export interface RatingData {
     comment: string;
     conversationId: number;
 }
+
+export interface ExistRatinResponse {
+    existExperience: boolean;
+}
+
 export const ratingService = {
   async submitRating(data: RatingData): Promise<void> {
     await api.post('/conversations/experience/create', data);
+  },
+  async getRating(conversationId: number): Promise<ExistRatinResponse> {
+    const response = await api.get(`/conversations/experience/exist?conversationId=${conversationId}`);
+    console.log("response.data in get experience:: ", response.data);
+    return response.data;
   }
 };
